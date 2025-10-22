987d944503231ef7b1c5ff5e9158d90a3ff8dccc
aneesarauf223-boop <aneesarauf223@gmail.com>
2025-10-22 20:37:48 +0500

    Add files via upload

---
+++ oparator 2.py
@@ -0,0 +1,7 @@
+Amount=int(input("please enter amount for withdraw :"))
+note_1=amount//100
+note_2=(Amount%100)//50
+((amount%100)%50//10)
+print("notes of 100 rupees" , note_1)
+print("notes of 50upees" , note_2)
+print("notes of 10rupees" , note_3
