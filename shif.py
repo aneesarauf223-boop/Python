c08608a98d52ac2bf1c5f746861999a508f835ba
aneesarauf223-boop <aneesarauf223@gmail.com>
2025-11-03 18:53:40 +0500

    Add files via upload

---
+++ shif.py
@@ -0,0 +1,10 @@
+a = 10
+b = -10
+# print bitwise right shift oparator
+print("a >> 1=", a >> 1)
+print("B >> 1 =", b >> 1 )
+a = 5
+b = 10
+# print bitwise left shift oparator
+print("a << 1=", a << 1)
+print("B << 1 =", b << 1 )
