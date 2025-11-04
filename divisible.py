9e2f6b30e3fe3a9829891b8ebb119d596d798a3e
aneesarauf223-boop <aneesarauf223@gmail.com>
2025-11-04 18:57:42 +0500

    Add files via upload

---
+++ divisible.py
@@ -0,0 +1,8 @@
+print("ENTER A NUMBER (NUMENATOR):")
+numn = int (input())
+print("enter a number (denominator): ")
+numd = (input())
+if numn%numd==0:
+    print("\n" +str(numn)+"is divisible by " +str(numd))
+else:
+    print("\n" +str(numn)+"is divisible by " +str(numd))
