bc59070a1ee4d81c4d205825dc2c49eeaf6e87d3
aneesarauf223-boop <aneesarauf223@gmail.com>
2025-11-04 18:46:03 +0500

    Add files via upload

---
+++ oparators.py
@@ -0,0 +1,15 @@
+V = 4 
+W = 5
+X = 8 
+Y = 2
+Z = 0
+Z = (V+W) * X/Y;
+print ("value of (v+w) * x/y is", Z)
+name = "alex"
+age = 0
+if name == "alex" or name == "john" and age >=2 :
+  print("hello! welcome ")
+else:
+  print("good bye !!")
+  
+
