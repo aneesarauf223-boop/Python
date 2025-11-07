99c3f0d1fb21baba062d3f12b664100ddd884e16
aneesarauf223-boop <aneesarauf223@gmail.com>
2025-11-07 18:14:09 +0500

    Add files via upload

---
+++ ELECTRICITY.py
@@ -0,0 +1,13 @@
+units = int(input("please enter number of units you consumed"))
+if (units< 50):
+    amount - units * 2.60
+    surcharge = 25
+elif(units <= 100):
+    amount = 130 + ((units - 50) * 3.25)
+    surcharge = 45
+    elif(units <= 200):
+amount = 130 +162.50 + ((units - 100) * 5.26)
+SURCHARG= 75
+total - amount + surcharge 
+print ("\nElectricity bill = %.2f" %total)
+
