abed74b718a5fdfcf23c3a0271d51f8edcfa0fac
aneesarauf223-boop <aneesarauf223@gmail.com>
2025-11-25 18:46:22 +0500

    Add files via upload

---
+++ floyds.py
@@ -0,0 +1,12 @@
+#take input from user 
+rows = int(input("please enter the total number of rows"))
+number = 1 #initialiise by 1
+print("floyd's triangle")
+#outer loop for number of rows
+for i in range(1, rows +  1):
+    #inner loop for number of columns
+      for j in range(1, i + 1):
+         #display result
+          print(number, end = '  ')
+          number = number +1 
+      print( )       
