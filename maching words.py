b0bca0f9c0d47db2358b351a84bf1051fdcd8ab8
aneesarauf223-boop <aneesarauf223@gmail.com>
2026-01-02 17:08:56 +0500

    Add files via upload

---
+++ maching words.py
@@ -0,0 +1,13 @@
+# function to  check whether
+# first and last chaaracter of words match
+def match_words(words):
+    ctr = 0
+    list = []
+    for word in words:
+        if len(word) > 1 and word[0] == word[-1]:
+                ctr += 1
+                list.append(word)
+    print("list of words with first and last character same\n", list)
+    return ctr
+count = match_words(['abc', 'cfc', 'xyz', 'aba', '1221'])
+print("number of words haveing first and last character same:", count)    
