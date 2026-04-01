8844ef8e81503fe1361dbd753f0c90edf636fb5b
aneesarauf223-boop <aneesarauf223@gmail.com>
2026-04-01 18:53:01 +0500

    Add files via upload

---
+++ wigid.py
@@ -0,0 +1,9 @@
+# import necessary libraries
+from tkinter import *
+# create window
+window = Tk()
+# set the window Title and Geometry
+window.title('Demo Window')
+window.geometry('400*300')
+# start the GUI event loop
+window.mainloop()
